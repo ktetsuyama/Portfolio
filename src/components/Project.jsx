fee1d08b64dd7bbd723569abb2c102ae90a909e4
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-06-11 20:06:45 -0700

    Added a new project to the portfolio page, added frameworks and libraries information.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,24 +2,34 @@ import React from "react";
 
 export const Project = ({ project }) => {
 	return (
-		<section className="card">
+		<section className="card transparent-card">
 			<header className="text-center">{project.projectName}</header>
-			<a
-				href={project.projectURL}
-				target="_blank"
-				rel="noopener noreferrer"
-				className="card-link d-block"
-			>
-				<img
-					src={project.projectImage}
-					alt={project.projectAlt}
-					style={{ width: "50%", height: "auto" }}
-				/>
-			</a>
+
+			<div className="text-center">
+				<a
+					href={project.projectURL}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="card-link"
+				>
+					<img
+						src={project.projectImage}
+						alt={project.projectAlt}
+						style={{ width: "50%", height: "auto" }}
+					/>
+				</a>
+			</div>
+
 			<p className="text-center mt-3">{project.projectDescription}</p>
-			<a href={project.projectRepo} className="text-center mt-3">
-				Link to the {project.projectName} GitHub repo
-			</a>
+			<p className="text-center">
+				This project uses the following languages and libraries:{" "}
+				{project.projectConcepts.join(", ")}
+			</p>
+			<p className="text-center mt-3">
+				<a href={project.projectRepo}>
+					Link to the {project.projectName} GitHub repo
+				</a>
+			</p>
 			<p className="text-center mt-3">
 				(or click the screenshot to visit the deployed app!)
 			</p>
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -2,57 +2,64 @@ import React from "react";
 import Project from "../Project";
 const projects = [
 	{
+		projectName: "RecallRocket",
+		projectURL: "https://recall-rocket.onrender.com/",
+		projectRepo: "https://github.com/ktetsuyama/recall-rocket",
+		projectDescription:
+			"This application is an app that allows users to study the concepts of the MERN stack, and create their own flashcards to further their understanding of the concepts.",
+		projectImage: "/recallrocket_screenshot.png",
+		projectAlt: "a website with flashcards for studying",
+		projectConcepts: ["React", "MongoDB", "Express", "& Node.js."],
+	},
+	{
 		projectName: "Vibe Check",
 		projectURL: "https://ktetsuyama.github.io/vibe-check/",
 		projectRepo: "https://github.com/ktetsuyama/Vibe_Check",
 		projectDescription:
-			"This application is an app that allows users to create playlists, rate their own playlists, and rate the playlists of other users",
+			"This application is an app that allows users to create playlists, rate their own playlists, and rate the playlists of other users.",
 		projectImage: "/vibecheck.png",
 		projectAlt: "a picture of a playlist of songs",
+		projectConcepts: ["Handlebars", "SQL", "Express", "& Node.js."],
 	},
 	{
 		projectName: "Holocron",
 		projectURL: "https://ktetsuyama.github.io/holocron/",
 		projectRepo: "https://github.com/ktetsuyama/Holocron",
-		projectDescription: "This application is a Star Wars themed search engine",
+		projectDescription:
+			"This application is a Star Wars themed search engine",
 		projectImage: "/holocron.png",
 		projectAlt: "a picture of a search engine with a Star Wars theme",
+		projectConcepts: ["HTML", "Javascript", "& CSS."],
 	},
 	{
 		projectName: "Janken",
 		projectURL: "https://ktetsuyama.github.io/Janken/",
 		projectRepo: "https://github.com/ktetsuyama/Janken",
-		projectDescription: "This application is a rock paper scissors game",
+		projectDescription: "This application is a rock paper scissors game.",
 		projectImage: "/janken_screenshot.png",
 		projectAlt:
 			"a picture of a javascript prompt box asking for input to play the game rock paper scissors",
+		projectConcepts: ["HTML", "Javascript", "& CSS."],
 	},
 	{
 		projectName: "Work Day Scheduler",
 		projectURL: "https://ktetsuyama.github.io/Work-Day-Scheduler/",
 		projectRepo: "https://github.com/ktetsuyama/Work-Day-Scheduler",
 		projectDescription:
-			"This application is an app that allows users to plan out their day, set tasks, and at a glance, see what part of day they are in and what task are coming up",
+			"This application is an app that allows users to plan out their day, set tasks, and at a glance, see what part of day they are in and what task are coming up.",
 		projectImage: "/Work-Day-Scheduler_.png",
 		projectAlt: "a day planner with time slots for each hour of the day",
+		projectConcepts: ["HTML", "Javascript", "& CSS."],
 	},
 	{
 		projectName: "Weather Dashboard",
 		projectURL: "https://ktetsuyama.github.io/Weather-Dashboard/",
 		projectRepo: "https://github.com/ktetsuyama/Weather-Dashboard",
 		projectDescription:
-			"This application is an app that allows users to check the weather in cities around the world. It shows a one day and a five day forecast",
+			"This application is an app that allows users to check the weather in cities around the world. It shows a one day and a five day forecast.",
 		projectImage: "/Weather-Dashboard.png",
 		projectAlt: "a weather forecast for Tokyo, Japan",
-	},
-	{
-		projectName: "Semantic Refactor",
-		projectURL: "https://ktetsuyama.github.io/semantic-refactor/",
-		projectRepo: "https://github.com/ktetsuyama/semantic-refactor",
-		projectDescription:
-			"This application is a refactor of a webpage to use semantic HTML elements",
-		projectImage: "/semanticrefactor.png",
-		projectAlt: "screenshot of a webpage with blue elements",
+		projectConcepts: ["HTML", "Javascript", "a third party API", "& CSS."],
 	},
 ];
 
